fe7c9362bbd1c47966b2b54384bd7e74997dc6a8
devdatt <shreebhattji@urmic.org>
2025-12-07 03:59:56 +0530

    rtmp_push0

--- encoder/static.php
+++ encoder/static.php
@@ -273,7 +273,7 @@ function update_service($which_service)
             $rtmp_push1 = "";
             for ($i = 1; $i <= 11; $i++) {
                 if ($rtmp_multiple1[$i]['enabled'] == 1) {
-                    $rtmp_push0 .= "
+                    $rtmp_push1 .= "
       push " . $rtmp_multiple1[$i]['url'] . ";";
                 }
             }
