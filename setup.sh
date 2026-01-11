a8eba83b87f0c8c06668e86e0ebaa3874b69662d
devdatt <shreebhattji@urmic.org>
2026-01-11 11:34:52 +0530

    update

--- setup.sh
+++ setup.sh
@@ -3,20 +3,20 @@ sudo cp -r html/* /var/www/html/
 sudo cp attempts.json /var/www/attempts.json
 DEVICE_ID="$(sudo cat /sys/class/dmi/id/product_uuid | tr -d '\n')"
 sudo sed -i 's/certificatecertificatecertificatecertificate/'$DEVICE_ID'/g' /var/www/html/certification.html
+
+SOURCE_FILE="users.json"
+TARGET_FILE="/var/www/users.json"
+
+if [ ! -f "$TARGET_FILE" ]; then
+    cp "$SOURCE_FILE" "$TARGET_FILE"
+fi
+
+SOURCE_FILE="users.json"
+TARGET_FILE="/var/www/users.json"
+
+if [ ! -f "$TARGET_FILE" ]; then
+    cp "$SOURCE_FILE" "$TARGET_FILE"
+fi
+
 sudo chown -R www-data:www-data /var/www/*
-
-SOURCE_FILE="users.json"
-TARGET_FILE="/var/www/users.json"
-
-if [ ! -f "$TARGET_FILE" ]; then
-    cp "$SOURCE_FILE" "$TARGET_FILE"
-fi
-
-SOURCE_FILE="users.json"
-TARGET_FILE="/var/www/users.json"
-
-if [ ! -f "$TARGET_FILE" ]; then
-    cp "$SOURCE_FILE" "$TARGET_FILE"
-fi
-
 sudo reboot
