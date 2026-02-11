79891a859ac6a965f18c883dcd4d6a37dbfe4c34
devdatt <shreebhattji@urmic.org>
2026-02-11 11:07:34 +0530

    update

--- encoder/firmware.php
+++ encoder/firmware.php
@@ -9,7 +9,7 @@ https://github.com/shreebhattji/Urmi/blob/main/licence.md
 */
 
 exec("sudo chmod 444 /sys/class/dmi/id/product_uuid");
-$version = 11.19;
+$version = 11.20;
 
 function fail(string $msg): never
 {
