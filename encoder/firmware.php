4c847a59c3afdcf6792bd7e4eb429b8af3202cad
Devdatt Bhatt <devdatt.bhatt@dbhatt.org>
2026-05-11 19:08:26 +0000

    removing gzip on meu8 and mpd files , to accomodate Access-Control-Allow-Origin "*"

--- encoder/firmware.php
+++ encoder/firmware.php
@@ -9,7 +9,7 @@ https://github.com/shreebhattji/Urmi/blob/main/licence.md
 */
 
 exec("sudo chmod 444 /sys/class/dmi/id/product_uuid");
-$version = "12.24";
+$version = "12.25";
 
 function fail(string $msg): never
 {
